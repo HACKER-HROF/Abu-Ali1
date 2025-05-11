d821d2f31d9d99a3e308a3c2285ebf673982192a
HACKER-HROF <hfhr39@gmail.com>
2025-05-11 09:30:56 +0300

    Update CPMHROF.py

--- CPMHROF.py
+++ CPMHROF.py
@@ -10,7 +10,7 @@ import requests
 __ENDPOINT_URL__: str = "https://popstool.io/api"
 
 
-class CPMFlanker:
+class CPMHROF:
     def __init__(self, access_key) -> None:
         self.auth_token = None
         self.access_key = access_key
